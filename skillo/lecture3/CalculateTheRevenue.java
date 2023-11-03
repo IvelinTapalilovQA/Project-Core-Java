ee1b2a65a8b43776cfdad2924aeb3f9d256b35b8
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-11-03 22:46:09 +0200

    Update Skillo lecture3 adding of CalculateTheRevenue

--- skillo/lecture3/CalculateTheRevenue.java
+++ skillo/lecture3/CalculateTheRevenue.java
@@ -6,46 +6,44 @@ public class CalculateTheRevenue {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter unit price: ");
         double unitPrice = input.nextDouble();
 
         if (unitPrice <= 0){
             System.out.println("You are entering an invalid unit price!");
             System.exit(0);
         }
+
+        System.out.println("Enter quantity: ");
         int quantity = input.nextInt();
+
+        if (quantity <= 0){
+            System.out.println("You are entering an invalid unit price!");
+            System.exit(0);
+        }
 
         double currentAmount = unitPrice * quantity;
 
         int discount15 = 15;
         int discount20 = 20;
-        double discountResult, amountAfterDiscount;
+        double discountAmount, revenue;
+        String revenueMessage = "The revenue from sale: ";
+        String discountMessage = "Discount: ";
 
         if (quantity >= 100 && quantity <= 120){
-            discountResult = (currentAmount * discount15) / 100;
-            amountAfterDiscount = currentAmount - discountResult;
-            System.out.println("Enter unit price: " + unitPrice);
-            System.out.println("Enter quantity: " + quantity);
-            System.out.println("The revenue from sale: " + amountAfterDiscount);
-            System.out.println("Discount: " + discountResult);
+            discountAmount = (currentAmount * discount15) / 100;
+            revenue = currentAmount - discountAmount;
         }
         else if (quantity > 120) {
-            discountResult = (currentAmount * discount20) / 100;
-            amountAfterDiscount = currentAmount - discountResult;
-            System.out.println("Enter unit price: " + unitPrice);
-            System.out.println("Enter quantity: " + quantity);
-            System.out.println("The revenue from sale: " + amountAfterDiscount);
-            System.out.println("Discount: " + discountResult);
-        }
-        else if (quantity > 0) {
-            discountResult = 0;
-            amountAfterDiscount = currentAmount;
-            System.out.println("Enter unit price: " + unitPrice);
-            System.out.println("Enter quantity: " + quantity);
-            System.out.println("The revenue from sale: " + amountAfterDiscount);
-            System.out.println("Discount: " + discountResult);
+            discountAmount = (currentAmount * discount20) / 100;
+            revenue = currentAmount - discountAmount;
         }
         else{
-            System.out.println("You are entering an invalid quantity!");
+            discountAmount = 0;
+            revenue = currentAmount;
         }
+
+        System.out.println(revenueMessage + revenue);
+        System.out.println(discountMessage + discountAmount);
     }
 }
