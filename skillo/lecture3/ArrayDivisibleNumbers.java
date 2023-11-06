6702d60769ccefbcc7ed8aa21b8752f6ac42cee2
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-11-06 23:31:12 +0200

    Update Skillo lecture3 adding of ArrayDivisibleNumbers

--- skillo/lecture3/ArrayDivisibleNumbers.java
+++ skillo/lecture3/ArrayDivisibleNumbers.java
@@ -3,9 +3,9 @@ package skillo.lecture3;
 public class ArrayDivisibleNumbers {
     public static void main(String[] args) {
 
-        int[] arrayNumbers = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
+        int[] arrayOfNumbers = {12, 15, 32, 42, 55, 75, 122, 132, 150, 180, 200};
 
-        for (int number : arrayNumbers) {
+        for (int number : arrayOfNumbers) {
             if (number % 5 == 0) {
                 System.out.println(number);
                 if (number > 150) {
