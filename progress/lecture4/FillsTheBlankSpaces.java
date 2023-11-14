7ef7fe168dda6d5fd9fbe258817496a7c89f0beb
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-11-15 01:53:44 +0200

    Update Progress lecture4 adding of FillsTheBlankSpaces

--- progress/lecture4/FillsTheBlankSpaces.java
+++ progress/lecture4/FillsTheBlankSpaces.java
@@ -1,6 +1,5 @@
 package progress.lecture4;
 
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class FillsTheBlankSpaces {
@@ -8,19 +7,11 @@ public class FillsTheBlankSpaces {
 
         Scanner input = new Scanner(System.in);
 
-        String firstWord = input.next();
-        String secondWord = input.next();
-        String thirdWord = input.next();
-
         String sentence = "My name is ____. I am ___ years old and I am teacher at \"_______\" school.";
 
-        LinkedList<String> listOfWords = new LinkedList<>();
-        listOfWords.add(firstWord);
-        listOfWords.add(secondWord);
-        listOfWords.add(thirdWord);
-
-        for (String word : listOfWords) {
-            sentence = sentence.replaceFirst("_+", word);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Please, enter a word: ");
+            sentence = sentence.replaceFirst("_+", input.nextLine());
         }
     System.out.println(sentence);
     }
