c502d8284c101bd1089544148e327eaa1cc5c1d8
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-11-15 02:32:19 +0200

    Update Progress lecture3 adding of DisplayTheMidChar

---
+++ skillo/methods/DisplayTheMidChar.java
@@ -0,0 +1,25 @@
+package skillo.methods;
+
+import java.util.Scanner;
+
+public class DisplayTheMidChar {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter a word: ");
+        String word = input.nextLine();
+
+        System.out.println(printMiddleChars(word));
+    }
+    static String printMiddleChars(String word){
+        String chars = "";
+        if (word.length() % 2 == 0) {
+            chars = word.substring(word.length() / 2 - 1, word.length() / 2 + 1);
+        } else {
+            chars += word.charAt(word.length() / 2);
+        }
+        System.out.print("The middle characters of the string are: ");
+        return chars;
+    }
+}
