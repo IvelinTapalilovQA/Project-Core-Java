33e520c7c45fdad34cfd53e1d8073b6d81e3fd83
IvelinTapalilovQA <119358506+IvelinTapalilovQA@users.noreply.github.com>
2023-11-15 16:01:07 +0200

    Update Progress collections adding of TwoDimArrayExercise

---
+++ progress/collections/TwoDimArrayExercise.java
@@ -0,0 +1,21 @@
+package progress.collections;
+
+import java.util.Arrays;
+
+public class TwoDimArrayExercise {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = new int[5][5];
+
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                    matrix[row][col] = row * matrix[col].length + col + 1;
+            }
+        }
+        for (int[] ints : matrix) {
+            System.out.println(Arrays.toString(ints));
+        }
+    }
+}
